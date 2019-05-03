97cbb9cdaeedd606094f64ad30d8fc847fc6810b
Emilia <majerzemilia@gmail.com>
2019-05-03 12:22:30 +0200

    added obstacles

--- match-runner-master/display.py
+++ match-runner-master/display.py
@@ -45,7 +45,7 @@ class PlayerObject:
         if self.on_the_ground() and not self.jumping:
             self.dy = 0
             self.y = 4 / 5 * HEIGHT - 20
-            self.max_jump_height = 4/5*HEIGHT - 150
+            self.max_jump_height = 4 / 5 * HEIGHT - 150
             self.falling = False
 
         self.y += self.dy * deltatime
@@ -65,7 +65,8 @@ class ScoreObject:
         font_surface = font.render(str(score), True, color.THECOLORS["black"], color.THECOLORS["gray"])
         self.surface.blit(font_surface, (5, 1 / 10 * HEIGHT))
 
-    def update(self, screen, tick):
+    def update(self, screen, tick, speed):
+        self.speed = speed
         self.rect = self.rect.move(-self.speed / tick, 0)
         screen.blit(self.surface, self.rect)
 
@@ -82,8 +83,84 @@ class RectangleObstacle:
         self.surface.fill(color.THECOLORS["blue"])
         self.rect = Rect(self.surface.get_rect())
 
-    def update(self, screen, tick):
+    def update(self, screen, tick, speed):
+        self.speed = speed
         self.rect = self.rect.move(-self.speed / tick, 0)
+        screen.blit(self.surface, self.rect)
+
+
+class Tunnel:
+
+    def __init__(self, game_speed):
+        self.x = WIDTH
+        self.tunnel_length = 250
+        self.upper_length = HEIGHT / 2 - 100 - randrange(10) * 15
+        self.lower_length = 4 / 5 * HEIGHT - self.upper_length - self.tunnel_length
+        self.speed = game_speed
+        self.upper_surface = pygame.Surface((25, self.upper_length))
+        self.upper_surface.fill(color.THECOLORS["green"])
+        self.lower_surface = pygame.Surface((25, self.lower_length))
+        self.lower_surface.fill(color.THECOLORS["green"])
+        self.upper_rect = Rect(self.upper_surface.get_rect())
+        self.lower_rect = Rect(self.lower_surface.get_rect())
+
+    def update(self, screen, tick, speed):
+        self.speed = speed
+        self.upper_rect = self.upper_rect.move(-self.speed / tick, 0)
+        self.lower_rect = self.lower_rect.move(-self.speed / tick, 0)
+        screen.blit(self.upper_surface, self.upper_rect)
+        screen.blit(self.lower_surface, self.lower_rect)
+
+
+class Stone:
+
+    def __init__(self, game_speed):
+        self.x = WIDTH
+        self.y = 0
+        self.dy = 0
+        self.delta = 3 + randrange(4)
+        self.speed = game_speed
+        self.surface = pygame.Surface((30, 30))
+        self.surface.fill(color.THECOLORS["yellow"])
+        self.rect = Rect(self.surface.get_rect())
+
+    def on_the_ground(self):
+        if self.y >= 4 / 5 * HEIGHT - 30:
+            return True
+        else:
+            return False
+
+    def on_the_top(self):
+        if self.y <= 0:
+            return True
+        else:
+            return False
+
+    def update(self, screen, tick, speed):
+        self.speed = speed
+        if self.on_the_top():
+            self.dy = self.delta
+        if self.on_the_ground():
+            self.y = 4 / 5 * HEIGHT - 30
+            self.dy = -self.delta
+        self.y += self.dy
+        self.rect = self.rect.move(-self.speed / tick, self.dy)
+        screen.blit(self.surface, self.rect)
+
+
+class GroundStone:
+
+    def __init__(self, game_speed):
+        self.x = WIDTH + 100
+        self.y = 4 / 5 * HEIGHT - 60
+        self.speed = 2.25 * game_speed
+        self.surface = pygame.Surface((60, 60))
+        self.surface.fill(color.THECOLORS["purple"])
+        self.rect = Rect(self.surface.get_rect())
+
+    def update(self, screen, tick, speed):
+        self.speed = 2.25 * speed
+        self.rect = self.rect = self.rect.move(-self.speed / tick, 0)
         screen.blit(self.surface, self.rect)
 
 
@@ -94,14 +171,17 @@ class Game:
         self._running = True
         self.speed = 50
         self.maxspeed = 150
+        self.final_score = 0
         self.player = PlayerObject()
         self.player.rect = self.player.rect.move(self.player.x, self.player.y)
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.scores = []
         self.scores_init()
-        #self.obstacles = []
         self.rectangle_obstacles = []
+        self.stones = []
+        self.ground_stones = []
+        self.tunnels = []
         self.deltaspeed = 0.01
         self.is_displaying_obstacles = False
 
@@ -112,20 +192,60 @@ class Game:
             self.scores.append(new_score)
 
     def scores_update(self):
-        for i in range(0, 9):
-            self.scores[i].speed = self.speed
         if self.scores[-1].rect.x < WIDTH - 100:
             new_score = ScoreObject(self.scores[-1].score + 100, self.speed)
             new_score.rect = new_score.rect.move(self.scores[-1].rect.x + 100, 4 / 5 * HEIGHT)
-            new_score.speed = self.scores[0].speed
             self.scores.append(new_score)
             self.scores.remove(self.scores[0])
+
+    def obstacles_display(self):
+        if not self.is_displaying_obstacles:
+            rand = randrange(4)
+            if rand == 0:
+                self.rectangle_obstacles_init()
+                self.is_displaying_obstacles = True
+            elif rand == 1:
+                self.stones_init()
+                self.is_displaying_obstacles = True
+            elif rand == 2:
+                self.ground_stone_init()
+                self.is_displaying_obstacles = True
+            elif rand == 3:
+                self.tunnels_init()
+                self.is_displaying_obstacles = True
+
+    def obstacles_handler(self, tick):
+        if self.rectangle_obstacles:
+            for obstacle in self.rectangle_obstacles:
+                obstacle.update(self.screen, tick, self.speed)
+                if obstacle.rect.colliderect(self.player.rect):
+                    self._running = False
+            self.rectangle_obstacles_update()
+        if self.stones:
+            for stone in self.stones:
+                stone.update(self.screen, tick, self.speed)
+                if stone.rect.colliderect(self.player.rect):
+                    self._running = False
+            self.stones_update()
+        if self.ground_stones:
+            for stone in self.ground_stones:
+                stone.update(self.screen, tick, self.speed)
+                if stone.rect.colliderect(self.player.rect):
+                    self._running = False
+            self.ground_stone_update()
+        if self.tunnels:
+            for tunnel in self.tunnels:
+                tunnel.update(self.screen, tick, self.speed)
+                if tunnel.upper_rect.colliderect(self.player.rect) or tunnel.lower_rect.colliderect(self.player.rect):
+                    self._running = False
+            self.tunnels_update()
 
     def rectangle_obstacles_init(self):
         for i in range(0, 6):
             if i % 2 == 0:
                 new_obstacle = RectangleObstacle(self.speed)
-                new_obstacle.rect = new_obstacle.rect.move(new_obstacle.x + i * 300, 4/5*HEIGHT - new_obstacle.height)
+                new_obstacle.rect = new_obstacle.rect.move(new_obstacle.x + i * 300,
+                                                           4 / 5 * HEIGHT - new_obstacle.height)
                 self.rectangle_obstacles.append(new_obstacle)
             else:
                 new_obstacle = RectangleObstacle(self.speed)
@@ -135,14 +255,47 @@ class Game:
     def rectangle_obstacles_update(self):
         if len(self.rectangle_obstacles) == 1:
             self.is_displaying_obstacles = False
-        for i in range(0, len(self.rectangle_obstacles)):
-            self.rectangle_obstacles[i].speed = self.speed
         if self.rectangle_obstacles[-1].rect.x <= -50:
             self.rectangle_obstacles.remove(self.rectangle_obstacles[0])
 
+    def tunnels_init(self):
+        for i in range(0, 3):
+            new_tunnel = Tunnel(self.speed)
+            new_tunnel.upper_rect = new_tunnel.upper_rect.move(new_tunnel.x + i * 375, 0)
+            new_tunnel.lower_rect = new_tunnel.lower_rect.move(new_tunnel.x + i * 375,
+                                                               4 / 5 * HEIGHT - new_tunnel.lower_length)
+            self.tunnels.append(new_tunnel)
+
+    def tunnels_update(self):
+        if len(self.tunnels) == 1:
+            self.is_displaying_obstacles = False
+        if self.tunnels[-1].upper_rect.x <= -50:
+            self.tunnels.remove(self.tunnels[0])
+
+    def stones_init(self):
+        for i in range(0, 5):
+            new_stone = Stone(self.speed)
+            new_stone.rect = new_stone.rect.move(new_stone.x + i * 250, new_stone.y)
+            self.stones.append(new_stone)
+
+    def stones_update(self):
+        if len(self.stones) == 1:
+            self.is_displaying_obstacles = False
+        if self.stones[-1].rect.x <= -30:
+            self.stones.remove(self.stones[0])
+
+    def ground_stone_init(self):
+        new_circle = GroundStone(self.speed)
+        new_circle.rect = new_circle.rect.move(new_circle.x, new_circle.y)
+        self.ground_stones.append(new_circle)
+
+    def ground_stone_update(self):
+        if self.ground_stones[0].rect.x <= -50:
+            self.ground_stones.remove(self.ground_stones[0])
+            self.is_displaying_obstacles = False
+
     def execute(self):
         while self._running:
-            print(self.speed)
             if self.speed < self.maxspeed:
                 self.speed += self.deltaspeed
             tick = self.clock.tick(60)
@@ -157,22 +310,19 @@ class Game:
                         self.player.jump(self.speed)
             self.screen.fill(color.THECOLORS["black"])
             for score in self.scores:
-                score.update(self.screen, tick)
+                score.update(self.screen, tick, self.speed)
 
-            if not self.is_displaying_obstacles:
-                rand = randrange(5)
-                if rand == 0:
-                    self.rectangle_obstacles_init()
-                    self.is_displaying_obstacles = True
-            if self.rectangle_obstacles:
-                for obstacle in self.rectangle_obstacles:
-                    if self.player.rect.colliderect(obstacle.rect):
-                        self._running = False
-                    obstacle.update(self.screen, tick)
-                self.rectangle_obstacles_update()
-            self.scores_update()
             self.player.update(tick / 1000.0, self.screen, self.speed)
+
+            self.obstacles_display()
+            self.obstacles_handler(tick)
+
+            self.scores_update()
             pygame.display.flip()
+        for score in self.scores:
+            if score.rect.left < self.player.rect.left:
+                self.final_score = score.score
+        print(self.final_score)
 
 
 def main():
