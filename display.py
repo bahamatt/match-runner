d5138d4e70fb0df17c84397488544ea3824c6b2f
Mateusz Wodecki <matwodecki@gmail.com>
2019-06-12 19:03:23 +0200

    Added help

--- display.py
+++ display.py
@@ -203,6 +203,7 @@ class Game:
                 self._high_scores = default_scores()
         self._start_window = True
         self._scores_window = False
+        self._help_window = False
         self._running = True
         self._paused = False
         self._gameover = False
@@ -450,6 +451,9 @@ class Game:
                 elif Rect(220, 292.5, 360, 82.5).colliderect(mouse_rect):
                     self._start_window = False
                     self._scores_window = True
+                elif Rect(220, 395, 360, 82.5).colliderect(mouse_rect):
+                    self._start_window = False
+                    self._help_window = True
                 elif Rect(220, 497.5, 360, 82.5).colliderect(mouse_rect):
                     pygame.quit()
                     quit()
@@ -480,6 +484,33 @@ class Game:
                 mouse_rect = pygame.Rect(pygame.mouse.get_pos(), (3, 3))
                 if Rect(30, HEIGHT - h - 20, w + 4, h + 4).colliderect(mouse_rect):
                     self._scores_window = False
+                    self._start_window = True
+
+    def help_screen(self):
+        self._running = False
+        self.screen.fill(color.THECOLORS["black"])
+        font = pygame.font.Font('dat/Century Gothic.ttf', 26)
+        with open("dat/helptext.txt", "r") as file:
+            i = 0
+            for line in file:
+                surface = font.render(line.strip(), True, color.THECOLORS["blue"])
+                self.screen.blit(surface, ((WIDTH / 16), (HEIGHT / 16) * i + 16))
+                i += 1
+        (w, h) = pygame.font.Font.size(font, "  Back to menu  ")
+        back_to_menu_surface = font.render("  Back to menu  ", True, color.THECOLORS["white"], color.THECOLORS["black"])
+        back_to_menu_bg = pygame.Surface((w + 4, h + 4))
+        back_to_menu_bg.fill(color.THECOLORS["gray"])
+        self.screen.blit(back_to_menu_bg, (28, HEIGHT - h - 22))
+        self.screen.blit(back_to_menu_surface, (30, HEIGHT - h - 20))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == MOUSEBUTTONDOWN:
+                mouse_rect = pygame.Rect(pygame.mouse.get_pos(), (3, 3))
+                if Rect(30, HEIGHT - h - 20, w + 4, h + 4).colliderect(mouse_rect):
+                    self._help_window = False
                     self._start_window = True
 
     def gameover(self):
@@ -532,6 +563,8 @@ class Game:
                 self.start_menu()
             while self._scores_window:
                 self.score_screen()
+            while self._help_window:
+                self.help_screen()
             while self._running:
                 if self.speed < self.maxspeed:
                     self.speed += self.deltaspeed
